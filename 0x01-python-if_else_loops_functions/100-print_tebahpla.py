5295c101e718af60b939f86a8b8fe3d93d4bef3a
abrhamadddis <abrhamaddis32@gmail.com>
2022-06-01 06:27:10 -0700

    advanced task

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i % 2 == 0:
-        i -= 32
-        print(chr(i), end="")
-    else:
-        print(chr(i), end="")
+for i in range(90, 64, -1):
+    print("{}".format(chr(i + 32) if i % 2 == 0 else chr(i)), end="")
