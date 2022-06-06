3b36898f1d5b40057c24ac42f067a022df31018c
abrhamadddis <abrhamaddis32@gmail.com>
2022-06-06 12:11:18 -0700

    mandatory task

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    for i in range(len(sentence)):
-        if (len(sentence)) == 0:
-            first_chr = None
-        else:
-            first_chr = sentence[0]
-            length = len(sentence)
-    tuples = (length, first_chr)
-    return (tuples)
+    length = len(sentence)
+    first = None if length == 0 else sentence[0]
+    return length, first
