b85271530430324668d0bd03ca662bf273e33c88
abrhamadddis <abrhamaddis32@gmail.com>
2022-06-21 07:36:38 -0700

    mandatory task

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''2-square.py: a script defines a square and check type and value errror'''
+
+
+class Square:
+    ''' create a square type'''
+
+    def __init__(self, size=0):
+        '''intialize square with size'''
+        self.__size = size
+
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        ''' Define the area of a square'''
+        product = self.__size * self.__size
+        return product
