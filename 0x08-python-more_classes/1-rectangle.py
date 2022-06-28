f7ec7ef4783ca1d8791a841203ba5f0c6960895d
abrhamadddis <abrhamaddis32@gmail.com>
2022-06-28 16:27:42 +0300

    mandatory task

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,6 +17,21 @@ class Rectangle:
         self.height = height
 
     @property
+    def height(self):
+        ''' a property to get height'''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        ''' a property to set the value of height'''
+        if type(value) is not int:
+            return TypeError('height must be an integer')
+        elif value < 0:
+            return ValueError('height must be >= 0')
+        else:
+            self.__height = value
+
+    @property
     def width(self):
         ''''
         a property to retirive width
@@ -27,23 +42,8 @@ class Rectangle:
     def width(self, value):
         ''' a property to set the value of width'''
         if type(value) is not int:
-            return TypeError("width must be an integer")
+            return TypeError('width must be an integer')
         elif value < 0:
-            return ValueError("width must be >= 0")
+            return ValueError('width must be >= 0')
         else:
             self.__width = value
-
-    @property
-    def height(self):
-        ''' a property to retirive height'''
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        ''' a property to set the value of height'''
-        if type(value) is not int:
-            return TypeError("height must be an integer")
-        elif value < 0:
-            return ValueError("height must be >= 0")
-        else:
-            self.__height = value
