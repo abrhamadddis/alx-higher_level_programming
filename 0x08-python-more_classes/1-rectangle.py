c735791fe743363b740cb9ec27432b83499adbff
abrhamadddis <abrhamaddis32@gmail.com>
2022-06-28 16:22:00 +0300

    mandatory task

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
-"""
+'''
 a python script that create a class callled Rectangle
-"""
+'''
 
 
 class Rectangle:
-    """
+    '''
     a class called Rectangle
     setter and getter function for width and height
-    """
+    '''
     def __init__(self, width=0, height=0):
-        """
+        '''
         instantiaion with optional width and height
-        """
+        '''
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """
+        ''''
         a property to retirive width
-        """
+        '''
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ a property to set the value of width"""
+        ''' a property to set the value of width'''
         if type(value) is not int:
             return TypeError("width must be an integer")
         elif value < 0:
@@ -35,12 +35,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """ a property to retirive height"""
+        ''' a property to retirive height'''
         return self.__height
 
     @height.setter
     def height(self, value):
-        """ a property to set the value of height"""
+        ''' a property to set the value of height'''
         if type(value) is not int:
             return TypeError("height must be an integer")
         elif value < 0:
