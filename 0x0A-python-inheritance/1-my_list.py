b1c8f65b21af2b5770c8a5b9bfe81c667de97f43
abrhamadddis <abrhamaddis32@gmail.com>
2022-07-04 20:44:48 +0300

    mandatory task

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+class MyList that inherits from list
+'''
+
+
+class MyList(list):
+    '''
+    class inhritaance
+    '''
+    def print_sorted(self):
+        print(sorted(self))
