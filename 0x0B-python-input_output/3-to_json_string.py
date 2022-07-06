afeb014bde0d5ce7691c086012e2060a4bfd67f6
abrhamadddis <abrhamaddis32@gmail.com>
2022-07-06 21:28:44 +0300

    mandatory task

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-# 5-
 '''
 a script that return the json representation of an object
 '''
+
 import json
 
 
-def to_json_string_(my_obj):
+def to_json_string(my_obj):
     '''
     a function that take my_obj as an argument
     and return json representation
     '''
-    
+
     return json.dumps(my_obj)
