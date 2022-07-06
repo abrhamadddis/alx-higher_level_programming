1839fb013f4d403c75b04af580095225f0221c00
abrhamadddis <abrhamaddis32@gmail.com>
2022-07-06 08:42:50 +0300

    mandatory task

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
+# 4-append_write.py
 '''
 a script that append a file to a script
 '''
 
 
-from asyncore import write
-
-
 def append_write(filename="", text=""):
     '''
     a functon that take filename and a string
-    that will be append to a file
+    as an argument
     '''
 
     with open(filename, 'a', encoding="utf-8") as f:
