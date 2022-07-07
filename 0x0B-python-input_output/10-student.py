c64589eb786fea9276eeb7cdbfb87d1c47bbe1ae
abrhamadddis <abrhamaddis32@gmail.com>
2022-07-07 08:41:25 +0300

    mandatoy task

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,37 +1,18 @@
-#!/usr/bin/pyton3
-"""Defiantion for a student class
-"""
+#!/usr/bin/python3
+"""Python function to create class Student"""
 
 
 class Student:
-   """a class for a student
-   """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-   def __init__(self, first_name, last_name, age):
-       """Initalizes a student object
-       """
-
-       self.first_name = first_name
-       self.last_name = last_name
-       self.age = age
-
-   def to_json(self, attrs=None):
-       """returns a json representatiion of
-       the object
-       """
-
-       if attrs != None and not isinstance(attrs, list):
-           raise TypeError("attrs must me a list of strings")
-
-       if attrs != None and any(not isinstance(attr, str) for attr in attrs):
-           raise TypeError("attrs must me a list of strings")
-
-       if attrs == None:
-           return self.__dict__
-
-       json_dic = {}
-       for key in attrs:
-           if key in self.__dict__.keys():
-               json_dic[key] = self.__dict__[key]
-
-       return json_dic
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        my_dict = dict()
+        for items in attrs:
+            if hasattr(self, items):
+                my_dict[items] = getattr(self, items)
+        return my_dict
