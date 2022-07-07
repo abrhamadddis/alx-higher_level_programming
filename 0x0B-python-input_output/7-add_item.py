de19a20568eb20c4d05507c707dbf0d2ea6b8151
abrhamadddis <abrhamaddis32@gmail.com>
2022-07-07 08:24:35 +0300

    mandatory task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-'''
-a script that adds all arguments to a python list and then save them to a file
-'''
+"""
+Python function that add arguments to a Python list
+"""
 import sys
-import json
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+import os
 
-args = sys.argv
-filename = "add_item.json"
-with open(filename, 'a+', encoding="utf-8") as f:
-    my_list = []
-    my_list.append(args[1:])
-    save_to_json_file(my_list, filename)
-    load_from_json_file(filename)
+save_json = __import__('7-save_to_json_file').save_to_json_file
+load_json = __import__('8-load_from_json_file').load_from_json_file
+
+my_list = []
+
+if not os.path.exists("./add_item.json"):
+    save_json(my_list, "add_item.json")
+my_list = load_json("add_item.json")
+
+for items in sys.argv[1:]:
+    my_list.append(items)
+save_json(my_list, "add_item.json")
