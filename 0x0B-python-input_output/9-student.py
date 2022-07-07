be332b7810eb7063f305b157d5341493455d4ad1
abrhamadddis <abrhamaddis32@gmail.com>
2022-07-07 08:35:40 +0300

    mandatory task

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -2,21 +2,21 @@
 """Defiens a student class
 that have to_json public method"""
 
+
 class Student:
-   """a class for a student
-   """
+    """a class for a student
+    """
 
-   def __init__(self, first_name, last_name, age):
-       """Initalizes a student object
-       """
+    def __init__(self, first_name, last_name, age):
+        """Initalizes a student object"""
 
-       self.first_name = first_name
-       self.last_name = last_name
-       self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-   def to_json(self):
-       """returns a json representatiion of
-       the object
-       """
+    def to_json(self):
+        """returns a json representatiion of
+        the object
+        """
 
-       return self.__dict__
+        return self.__dict__
