848428f2d7c5aac7af3284f895ed41c74e4e9ce3
abrhamadddis <abrhamaddis32@gmail.com>
2022-07-10 00:43:55 +0300

    mandator task

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""class base"""
+
+
+class Base:
+    '''base class with private attributes'''
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        '''instatatiation id for base class'''
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
