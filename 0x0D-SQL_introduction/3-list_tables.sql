5bf47662c5f15058d85228fd184ee9777ecc8adc
abrhamadddis <abrhamaddis32@gmail.com>
2022-08-11 23:06:53 +0300

    creating first table

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- a script that list the table of mysql database serve
+USE mysql;
+SHOW TABLES;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- a script that create a table called first table 
+-- the data base  name will be passed as the argument of mysql command
+USE mysql
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
