6c9cd792c8c0200bfd007bf6fd09397d195767ec
abrhamadddis <abrhamaddis32@gmail.com>
2022-09-07 07:46:58 +0300

    an integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,8 +3,8 @@
 const argv = process.argv;
 let argv1;
 
-if (argv[2]) {
-  if (!isNaN(parseInt(argv[2]))) {
+if (argv[2] != null) {
+  if (!isNaN(parseInt(argv[2])) === true) {
     argv1 = parseInt(argv[2]);
     console.log('My number:', argv1);
   } else {
