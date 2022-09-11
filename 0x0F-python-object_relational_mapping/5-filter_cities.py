e28dcea3a4a5dd85ce04767ff8670c221d221dd1
abrhamadddis <abrhamaddis32@gmail.com>
2022-09-11 08:57:31 +0300

    all city by states

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,27 +1,15 @@
 #!/usr/bin/python3
-"""
-A script that takes in the name of a state as an argument
-and lists all cities of that state, using the database hbtn_0e_4_usa
-"""
-import sys
+'''a script that takes in the name of a state as an argument and
+lists all cities of that state, using the database hbtn_0e_4_usa'''
 import MySQLdb
+from sys import argv
 
-
-if __name__ == '__main__':
-    user = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state = sys.argv[4]
-
-    db = MySQLdb.connect(user=user, passwd=password, db=db_name)
-    cur = db.cursor()
-
-    cur.execute("SELECT c.name \
-                FROM cities c INNER JOIN states s \
-                ON c.state_id = s.id WHERE s.name = {} \
-                ORDER BY c.id".format(state))
-    cities = cur.fetchall()
-
-    for i in range(len(cities)):
-        print(cities[i][0], end=", " if i < len(cities) - 1 else "")
-    print()
+if __name__ == "__main__":
+    db_con = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                             passwd=argv[2], db=argv[3],
+                             charset="utf8")
+    curr = db_con.cursor()
+    curr.execute("SELECT cities.name FROM cities JOIN states ON \
+    cities.state_id = states.id WHERE states.name LIKE %s;", (argv[4],))
+    cities = curr.fetchall()
+    print(", ".join(city[0] for city in cities))
