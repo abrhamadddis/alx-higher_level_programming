ce64b9df99eaaa963f98734e2d852d59405cf7e2
abrhamadddis <abrhamaddis32@gmail.com>
2022-09-24 13:41:38 +0300

    what is my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,11 @@
 """
     it's python script  that fetches from https://alx-intranet.hbtnn.io.status
 """
-
-import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
-    html = r.read()
-    print('Body response:')
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format((html)))
-    print("\t- utf8 content: {}".format(html.decode('utf-8')))
+if __name__ == "__main__":
+    import urllib.request
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as file:
+        html = file.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format((html)))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
