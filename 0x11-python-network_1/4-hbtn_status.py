47887ed821a72f880998be915b020c0288b72e76
abrhamadddis <abrhamaddis32@gmail.com>
2022-09-29 12:42:53 +0300

    what is my status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''a Python script that fetches https://alx-intranet.hbtn.io/status
+with package requests
+'''
+
+import requests
+
+if __name__ == "__main__":
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
